b2991fe31c1269042f13b3f07c1be9f0ae87cf58
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-12-14 16:51:50 +0100

    Bump version to 1.1.0

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 1.1.0 - 2014-12-14
+* Fully functional build process  
+
 #### 1.0.3 - 2014-12-14
 * Initial release
--- src/CalculatorService/AssemblyInfo.fs
+++ src/CalculatorService/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("CalculatorService")>]
 [<assembly: AssemblyProductAttribute("CalculatorService")>]
 [<assembly: AssemblyDescriptionAttribute("Demo of F# Suave based Service Fabric stateless service")>]
-[<assembly: AssemblyVersionAttribute("1.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.3")>]
+[<assembly: AssemblyVersionAttribute("1.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.3"
+    let [<Literal>] Version = "1.1.0"
